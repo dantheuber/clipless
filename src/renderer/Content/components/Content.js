577ca94ee1463e883d441ecb81aab6ef52b51028
dantheuber <dan.essig@gmail.com>
2019-11-03 15:32:55 -0700

    working app

---
+++ src/renderer/Content/components/Content.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+export const Content = () => (
+  <div>Testing 1 2 3</div>
+);
+Content.displayName = 'Content';
---
+++ src/renderer/store.js
@@ -0,0 +1,38 @@
+import {
+  combineReducers,
+  createStore,
+  applyMiddleware,
+} from 'redux';
+import reduxThunk from 'redux-thunk';
+import reduxLogger from 'redux-logger';
+import reducers from './reducers';
+import { LOCAL_STORAGE_STATE_KEY } from './constants';
+
+const storedState = JSON.parse(sessionStorage.getItem(LOCAL_STORAGE_STATE_KEY)) || {};
+
+const middleware = [
+  reduxThunk,
+];
+
+// create-react-app build overrides NODE_ENV with "production"
+// so we use our own env variable for development builds.
+if (['development', 'dev', 'local'].includes(process.env.NODE_ENV)) {
+  middleware.push(reduxLogger);
+}
+
+const store = createStore(
+  combineReducers({ ...reducers }),
+  storedState,
+  applyMiddleware(...middleware),
+);
+
+// stores redux state in session storage
+window.addEventListener('beforeunload', () => {
+  const saveState = {
+    ...store.getState(),
+  };
+  sessionStorage.setItem(LOCAL_STORAGE_STATE_KEY, JSON.stringify(saveState));
+});
+
+
+export default store;
