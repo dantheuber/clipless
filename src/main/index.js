4ce04eec3e3bfc1a69859b112449530e91b2afa3
dantheuber <dan.essig@gmail.com>
2019-11-10 13:34:06 -0700

    remove main process event listener

--- src/main/index.js
+++ src/main/index.js
@@ -100,10 +100,6 @@ function registerShortcuts(window) {
   });
 }
 
-app.on('browser-window-blur', (event, window) => {
-  window.webContents.send('window-blur');
-});
-
 // quit application when all windows are closed
 app.on('window-all-closed', () => {
   // on macOS it is common for applications to stay open until the user explicitly quits
