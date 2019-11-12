31d51e1f88950e9f8caf7ef6cd48e7a4646bc134
dantheuber <dan.essig@gmail.com>
2019-11-11 20:56:06 -0700

    remove commented out webpack config

--- config/webpack.renderer.config.js
+++ config/webpack.renderer.config.js
@@ -5,19 +5,6 @@ const MONACO_DIR = path.resolve(__dirname, './node_modules/monaco-editor');
 module.exports = {
   module: {
     rules: [
-      // {
-      //   test: /\.css$/,
-      //   include: APP_DIR,
-      //   use: [{
-      //     loader: 'style-loader',
-      //   }, {
-      //     loader: 'css-loader',
-      //     options: {
-      //       modules: true,
-      //       namedExport: true,
-      //     },
-      //   }],
-      // },
       {
         test: /\.css$/,
         include: MONACO_DIR,
