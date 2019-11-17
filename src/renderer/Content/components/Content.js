14a65d9de17de68f955db0d8abb2faf59709246d
dantheuber <dan.essig@gmail.com>
2019-11-16 21:00:12 -0700

    do not need transparent class

--- src/renderer/Content/components/Content.js
+++ src/renderer/Content/components/Content.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import classnames from 'classnames';
 import Container from 'react-bootstrap/Container';
 import { Clips } from '../../clips';
 import { Header } from '../../header';
 import { Preferences } from '../../preferences';
 
-export const Content = ({ viewingPreferences, transparent }) => (
-  <Container fluid className={classnames('wrapper', {
-    transparent,
-  })}>
+export const Content = ({ viewingPreferences }) => (
+  <Container fluid className="wrapper">
     <Header />
     { viewingPreferences && <Preferences /> }
     { !viewingPreferences && <Clips /> }
@@ -17,6 +14,5 @@ export const Content = ({ viewingPreferences, transparent }) => (
 );
 Content.propTypes = {
   viewingPreferences: PropTypes.bool.isRequired,
-  transparent: PropTypes.bool.isRequired,
 };
 Content.displayName = 'Content';
--- src/renderer/content/containers/Content.js
+++ src/renderer/content/containers/Content.js
@@ -1,10 +1,9 @@
 import { connect } from 'react-redux';
-import { viewingPreferences, transparent } from '../../preferences/selectors';
+import { viewingPreferences } from '../../preferences/selectors';
 import { Content } from '../components/Content';
 
 const mapStateToProps = state => ({
   viewingPreferences: viewingPreferences(state),
-  transparent: transparent(state),
 });
 
 const container = connect(mapStateToProps)(Content);
