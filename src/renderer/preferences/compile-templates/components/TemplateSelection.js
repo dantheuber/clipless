e4be0e59e67b5a919c10f4163cac628bf451ca76
dantheuber <dan.essig@gmail.com>
2019-11-30 18:33:01 -0700

    some style stuff and tab selections

--- src/renderer/preferences/compile-templates/components/TemplateSelection.js
+++ src/renderer/preferences/compile-templates/components/TemplateSelection.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import ListGroup from 'react-bootstrap/ListGroup';
 
 export const TemplateSelection = ({
   compileTemplates,
   selectTemplate,
 }) => (
-  <ol>
-    { compileTemplates.map(template => (
-      <li
+  <ListGroup className="dark">
+    { compileTemplates.map((template, index) => (
+      <ListGroup.Item
+        action
+        variant="dark"
+        tabIndex={index}
         key={template.id}
         onClick={() => selectTemplate(template)}
       >
         { template.name }
-      </li>
+      </ListGroup.Item>
     ))}
-  </ol>
+  </ListGroup>
 );
 TemplateSelection.propTypes = {
   compileTemplates: PropTypes.arrayOf(PropTypes.object).isRequired,
