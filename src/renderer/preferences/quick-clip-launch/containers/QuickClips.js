d4e91c2b1794a47a59d7f4e92993adf2ed8f1d82
Dantheuber <dan.essig@gmail.com>
2021-09-21 22:50:12 -0700

    more cleanup

--- src/renderer/preferences/quick-clip-launch/containers/QuickClips.js
+++ src/renderer/preferences/quick-clip-launch/containers/QuickClips.js
@@ -1,23 +1,13 @@
 import { connect } from 'react-redux';
-import {
-  createNewTool,
-  deleteTool,
-  toggleAutoScan,
-} from '../actions';
-import {
-  autoScan,
-  tools
-} from '../selectors';
+import { toggleAutoScan } from '../actions';
+import { autoScan } from '../selectors';
 import { QuickClips } from '../components/QuickClips';
 
 const mapStateToProps = state => ({
   autoScan: autoScan(state),
-  tools: tools(state),
 });
 const mapDispatchToProps = {
   toggleAutoScan,
-  createNewTool,
-  deleteTool,
 };
 
 const container = connect(mapStateToProps, mapDispatchToProps)(QuickClips);
